0b56d8d88e63c95e8786502fe7a35e1623a2f94d
kishps <kishps1@gmail.com>
2021-05-26 22:51:41 +0300

    добавление стадий задач и фоматирование документа

--- report.js
+++ report.js
@@ -2,7 +2,7 @@ class ReportTable {
 
     selector = '.report-table'; //контейнер отчета
 
-    objShowFields = {title:"Задача",durationFact:"Затраченное время", closedDate:'дата закрытия'};
+    objShowFields = { title: "Задача", durationFact: "Затраченное время", closedDate: 'Дата закрытия', subStatus: 'Cтатус' };
 
     constructor(selector) {
         if (selector) this.selector = selector; // контейнер отчета
@@ -61,9 +61,9 @@ class ReportTable {
                 let divGroup = that.addDivGroup(groupKey);
                 $(that.selector).append(divGroup);
                 if ($(`.infobar[data-id="${groupKey}"]`).data('progress'))
-                $(`.chart-plan[data-id="${groupKey}"]`).progressbar({
-                    value: $(`.infobar[data-id="${groupKey}"]`).data('progress')
-                });
+                    $(`.chart-plan[data-id="${groupKey}"]`).progressbar({
+                        value: $(`.infobar[data-id="${groupKey}"]`).data('progress')
+                    });
             }
         });
     }
@@ -101,10 +101,10 @@ class ReportTable {
     infobar(groupKey) {
         let totalTasks = this.groups[groupKey].count;
         let totalClosedTasks = this.tasks[groupKey].reduce(function (accumulator, task) {
-            if (task.closedDate) {return accumulator + 1;} 
-            else {return accumulator + 0;} 
+            if (task.closedDate) { return accumulator + 1; }
+            else { return accumulator + 0; }
         }, 0);
-        
+
         let timeTariff = (this.groups[groupKey].timeTariff) ? this.groups[groupKey].timeTariff : '';
         let timeTarifffinaly = '';
         if (timeTariff) {
@@ -118,13 +118,13 @@ class ReportTable {
         let timeLeftOver = '';
         let progressVal = '';
         if (timeTariff && timeTariff - timeDurationFact > 0) {
-            progressVal = `data-progress="${timeDurationFact/timeTariff*100}"`;
+            progressVal = `data-progress="${timeDurationFact / timeTariff * 100}"`;
             timeLeftOver = `<div class="infobar-label">
                                     <span class="label-name">Осталось</span>
                                     ${this.minToHour(timeTariff - timeDurationFact)}
                                 </div>`;
         } else if (timeTariff && timeTariff - timeDurationFact < 0) {
-            progressVal = `data-progress="${timeDurationFact/timeTariff*100}"`;
+            progressVal = `data-progress="${timeDurationFact / timeTariff * 100}"`;
             timeLeftOver = `<div class="infobar-label">
                                     <span class="label-name">Перерасход</span>
                                     ${this.minToHour(timeDurationFact - timeTariff)}
@@ -132,7 +132,7 @@ class ReportTable {
         } else if (timeTariff && timeTariff - timeDurationFact == 0) {
             progressVal = `data-progress="100"`;
         }
-        console.log('timeTariff',timeTariff);
+        console.log('timeTariff', timeTariff);
         return `<div data-id="${groupKey}" class="infobar" ${progressVal}>
                     <div class="infobar-label">
                         <span class="label-name">Всего задач</span>
@@ -153,50 +153,167 @@ class ReportTable {
 
     getTbody(groupKey) {
         let arrTasksGroup = this.tasks[groupKey];
-        console.log('arrTasksGroup',arrTasksGroup);
-        let arrTd='';
+        console.log('arrTasksGroup', arrTasksGroup);
+        let arrTd = '';
         let objShowFields = this.objShowFields;
-        arrTasksGroup.map((task) => {   
-                                        arrTd += '<tr>';
-                                        Object.keys(objShowFields).map((field) => {
-                                            let td;
-                                            switch(field){
-                                                case 'title':
-                                                  td = `<td data-field="${field}"><a target="_blank" href="https://csn.bitrix24.ru/workgroups/group/${task.group.id}/tasks/task/view/${task.id}/">${task[field]}</a></td>`;
-                                                  break;
-                                                case 'durationFact':
-                                                case 'durationPlan':
-                                                case 'timeEstimate':
-                                                  td = `<td data-field="${field}">${this.minToHour(task[field])}</td>`;
-                                                  break;
-                                                case 'changedDate':
-                                                case 'closedDate':
-                                                case 'createdDate':
-                                                case 'deadline':
-                                                case 'statusChangedDate':
-                                                case 'viewedDate':
-                                                    td = `<td data-field="${field}">${new Date(task[field]).toLocaleDateString()}</td>`;
-                                                  break;
-                                                default:
-                                                    td = `<td data-field="${field}">${task[field]}</td>`;
-                                              }
-                                            arrTd += td;
-                                            });
-                                        arrTd += '</tr>';
-                                    });
-        return arrTd+'';
+        arrTasksGroup.map((task) => {
+            arrTd += '<tr>';
+            Object.keys(objShowFields).map((field) => {
+                let td;
+                switch (field) {
+                    case 'title':
+                        td = `<td data-field="${field}"><a target="_blank" href="https://csn.bitrix24.ru/workgroups/group/${task.group.id}/tasks/task/view/${task.id}/">${task[field]}</a></td>`;
+                        break;
+                    case 'durationFact':
+                    case 'durationPlan':
+                    case 'timeEstimate':
+                        td = `<td data-field="${field}">${this.minToHour(task[field])}</td>`;
+                        break;
+                    case 'changedDate':
+                    case 'closedDate':
+                    case 'createdDate':
+                    case 'deadline':
+                    case 'statusChangedDate':
+                    case 'viewedDate':
+                        td = `<td data-field="${field}">${(task[field])?new Date(task[field]).toLocaleDateString():''}</td>`;
+                        break;
+                    case 'status':
+                    case 'subStatus':
+                        let status = task[field];
+                        let statusColor = '';
+                        switch (status) {
+                            case '-3':
+                                statusColor = "#fba50b";
+                                td = `<td title="Задача почти просрочена" data-status="${status}" data-field="${field}"><svg width="154" height="21" viewBox="0 0 154 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <g class="status">
+                                                        <rect class="status56" x="0.5" y="0.5" width="153" height="20" fill="#C4C4C4" stroke="black"/>
+                                                        <rect class="status12" x="0.5" y="0.5" width="21" height="20" fill="${statusColor}" stroke="black"/>
+                                                        <rect class="status3" x="21.5" y="0.5" width="35" height="20" fill="#C4C4C4" stroke="black"/>
+                                                        <rect class="status4" x="56.5" y="0.5" width="35" height="20" fill="#C4C4C4" stroke="black"/>
+                                                        </g>
+                                                        </svg>
+                                                    </td>`;
+                                break;
+                            case '-2':
+                                statusColor = "#efc100";
+                                td = `<td title="Не просмотренная задача" data-status="${status}" data-field="${field}"><svg width="154" height="21" viewBox="0 0 154 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <g class="status">
+                                                        <rect class="status56" x="0.5" y="0.5" width="153" height="20" fill="#C4C4C4" stroke="black"/>
+                                                        <rect class="status12" x="0.5" y="0.5" width="21" height="20" fill="${statusColor}" stroke="black"/>
+                                                        <rect class="status3" x="21.5" y="0.5" width="35" height="20" fill="#C4C4C4" stroke="black"/>
+                                                        <rect class="status4" x="56.5" y="0.5" width="35" height="20" fill="#C4C4C4" stroke="black"/>
+                                                        </g>
+                                                        </svg>
+                                                    </td>`;
+                                break;
+                            case '-1':
+                                statusColor = "#df412d";
+                                td = `<td title="Задача просрочена" data-status="${status}" data-field="${field}"><svg width="154" height="21" viewBox="0 0 154 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <g class="status">
+                                                        <rect class="status56" x="0.5" y="0.5" width="153" height="20" fill="#C4C4C4" stroke="black"/>
+                                                        <rect class="status12" x="0.5" y="0.5" width="21" height="20" fill="${statusColor}" stroke="black"/>
+                                                        <rect class="status3" x="21.5" y="0.5" width="35" height="20" fill="#C4C4C4" stroke="black"/>
+                                                        <rect class="status4" x="56.5" y="0.5" width="35" height="20" fill="#C4C4C4" stroke="black"/>
+                                                        </g>
+                                                        </svg>
+                                                    </td>`;
+                                break;
+                            case '2':
+                                statusColor = "#50ffe6";
+                                td = `<td title="Ждет выполнения" data-status="${status}" data-field="${field}"><svg width="154" height="21" viewBox="0 0 154 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <g class="status">
+                                                        <rect class="status56" x="0.5" y="0.5" width="153" height="20" fill="#C4C4C4" stroke="black"/>
+                                                        <rect class="status12" x="0.5" y="0.5" width="21" height="20" fill="${statusColor}" stroke="black"/>
+                                                        <rect class="status3" x="21.5" y="0.5" width="35" height="20" fill="#C4C4C4" stroke="black"/>
+                                                        <rect class="status4" x="56.5" y="0.5" width="35" height="20" fill="#C4C4C4" stroke="black"/>
+                                                        </g>
+                                                        </svg>
+                                                    </td>`;
+                                break;
+                            case '3':
+                                statusColor = "#0800ff";
+                                td = `<td title="Выполняется" data-status="${status}" data-field="${field}"><svg width="154" height="21" viewBox="0 0 154 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <g class="status">
+                                                        <rect class="status56" x="0.5" y="0.5" width="153" height="20" fill="#C4C4C4" stroke="black"/>
+                                                        <rect class="status12" x="0.5" y="0.5" width="21" height="20" fill="${statusColor}" stroke="black"/>
+                                                        <rect class="status3" x="21.5" y="0.5" width="35" height="20" fill="${statusColor}" stroke="black"/>
+                                                        <rect class="status4" x="56.5" y="0.5" width="35" height="20" fill="#C4C4C4" stroke="black"/>
+                                                        </g>
+                                                        </svg>
+                                                    </td>`;
+                                break;
+                            case '4':
+                                statusColor = "#4ba984";
+                                td = `<td title="Ожидает контроля" data-status="${status}" data-field="${field}"><svg width="154" height="21" viewBox="0 0 154 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <g class="status">
+                                                        <rect class="status56" x="0.5" y="0.5" width="153" height="20" fill="#C4C4C4" stroke="black"/>
+                                                        <rect class="status12" x="0.5" y="0.5" width="21" height="20" fill="${statusColor}" stroke="black"/>
+                                                        <rect class="status3" x="21.5" y="0.5" width="35" height="20" fill="${statusColor}" stroke="black"/>
+                                                        <rect class="status4" x="56.5" y="0.5" width="35" height="20" fill="${statusColor}" stroke="black"/>
+                                                        </g>
+                                                        </svg>
+                                                    </td>`;
+                                break;
+                            case '5':
+                                statusColor = "#00ff10";
+                                td = `<td title="Завершена" data-status="${status}" data-field="${field}"><svg width="154" height="21" viewBox="0 0 154 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <g class="status">
+                                                        <rect class="status56" x="0.5" y="0.5" width="153" height="20" fill="${statusColor}" stroke="black"/>
+                                                        <rect class="status12" x="0.5" y="0.5" width="21" height="20" fill="${statusColor}" stroke="black"/>
+                                                        <rect class="status3" x="21.5" y="0.5" width="35" height="20" fill="${statusColor}" stroke="black"/>
+                                                        <rect class="status4" x="56.5" y="0.5" width="35" height="20" fill="${statusColor}" stroke="black"/>
+                                                        </g>
+                                                        </svg>
+                                                    </td>`;
+                                break;
+                            case '6':
+                                statusColor = "#C4C4C4";
+                                td = `<td title="Отложена" data-status="${status}" data-field="${field}"><svg width="154" height="21" viewBox="0 0 154 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <g class="status">
+                                                        <rect class="status56" x="0.5" y="0.5" width="153" height="20" fill="${statusColor}" stroke="black"/>
+                                                        <rect class="status12" x="0.5" y="0.5" width="21" height="20" fill="${statusColor}" stroke="black"/>
+                                                        <rect class="status3" x="21.5" y="0.5" width="35" height="20" fill="${statusColor}" stroke="black"/>
+                                                        <rect class="status4" x="56.5" y="0.5" width="35" height="20" fill="${statusColor}" stroke="black"/>
+                                                        </g>
+                                                        </svg>
+                                                    </td>`;
+                                break;
+                            default:
+                                statusColor = "#C4C4C4";
+                                td = `<td data-status="${status}" data-field="${field}"><svg width="154" height="21" viewBox="0 0 154 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <g class="status">
+                                                        <rect class="status56" x="0.5" y="0.5" width="153" height="20" fill="${statusColor}" stroke="black"/>
+                                                        <rect class="status12" x="0.5" y="0.5" width="21" height="20" fill="${statusColor}" stroke="black"/>
+                                                        <rect class="status3" x="21.5" y="0.5" width="35" height="20" fill="${statusColor}" stroke="black"/>
+                                                        <rect class="status4" x="56.5" y="0.5" width="35" height="20" fill="${statusColor}" stroke="black"/>
+                                                        </g>
+                                                        </svg>
+                                                    </td>`;
+                        }
+                        
+                        break;
+                    default:
+                        td = `<td data-field="${field}">${task[field]}</td>`;
+                }
+                arrTd += td;
+            });
+            arrTd += '</tr>';
+        });
+        return arrTd + '';
     }
 
     getThead() {
-        let arrTh='';
+        let arrTh = '';
         let objShowFields = this.objShowFields;
-        Object.keys(objShowFields).map((field) => {arrTh +=`
-                                                            <th data-field="${field}">${objShowFields[field]}</th>`});
+        Object.keys(objShowFields).map((field) => {
+            arrTh += `
+                                                            <th data-field="${field}">${objShowFields[field]}</th>`
+        });
         return arrTh;
     }
 
     detailInfo(groupKey) {
-        let tbody= this.getTbody(groupKey);
+        let tbody = this.getTbody(groupKey);
         let thead = this.getThead();
         return `<div class="table">
                     <table data-id="${groupKey}">
